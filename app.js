894f65e3f55afb4984f2fa938dd7ce33272ecccb
Mariaa98 <44316955+Mariaa98@users.noreply.github.com>
2021-02-03 16:21:34 +0200

    Add files via upload
    Complete API

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true});
+
+const articleSchema = {
+  title:String,
+  content:String
+}
+const Article = mongoose.model("Article",articleSchema); //if the collection not here it will create
+/////////////////////////////////////////////Requests Targting all Articles /////////////////////////
+app.route("/articles")
+.get(function(req,res){
+  Article.find({},function(err,foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    }else{
+      res.send(err);
+    }
+  });
+})
+.post(function(req,res){
+
+  const newArticle = new Article({
+    title:req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Succefully added new article. ");
+    }else{
+      res.send(err);
+    }
+  });
+})
+.delete(function(req,res){
+  Article.deleteMany({},function(err){
+    if(!err){
+      res.send("Successfully deleted all articles ")
+    }else{
+      res.send(err);
+    }
+  });
+});
+
+/////////////////////////////////////////////Requests Targting spcefic Article /////////////////////////
+app.route("/articles/:articleTitle")
+.get(function(req,res){
+  Article.findOne({title:req.params.articleTitle},function(err,foundArticle){
+    if(foundArticle){
+      res.send(foundArticle);
+    }else{
+      res.send("No articles matching that title was found");
+    }
+  });
+})
+.put(function(req,res){
+  Article.update(
+    {title:req.params.articleTitle},
+    {title:req.body.title,content:req.body.content},
+    {overwrite:true},
+    function(err){
+      if(!err){
+        res.send("Succefully updated article !");
+      }
+    }
+  );
+})
+.patch(function(req,res){
+  Article.update(
+    {title:req.params.articleTitle},
+    {$set: req.body },
+    function(err){
+      if(!err){
+        res.send("Succefully updated article");
+      }else{
+        res.send(err);
+      }
+    }
+  );
+})
+.delete(function(req,res){
+  Article.deleteOne(
+    {title:req.params.articleTitle},
+    function(err){
+      if(!err){
+        res.send("Successfully Deleted the article .")
+      }else{
+        res.send(err);
+      }
+    }
+  );
+});
+app.listen(process.env.PORT||3000,function(){
+  console.log("Server is up and running");
+})
